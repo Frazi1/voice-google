043b18ee19d71bba30d3111d24190de2ffd8fb09
fraz <diman896@gmail.com>
2018-05-22 21:30:07 +0300

    VariableResolver improvements

--- GoogleSpeechApi/TextProcessing/VariableResolver.cs
+++ GoogleSpeechApi/TextProcessing/VariableResolver.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using GoogleSpeechApi.Context;
 using GoogleSpeechApi.Context.Provider;
 using GoogleSpeechApi.DI.Interfaces;
+using GoogleSpeechApi.Extensions;
 using GoogleSpeechApi.TextProcessing.Interfaces;
 using JetBrains.Annotations;
 
@@ -16,6 +17,8 @@ namespace GoogleSpeechApi.TextProcessing
         private readonly IStringDistanceEvaluator _distanceEvaluator;
         private readonly IVariableProvider _variableProvider;
         private readonly ITextTransliterator _transliterator;
+
+        private const int MaxPlainTextAllowedDistance = 3;
 
         public VariableResolver(ILogger logger, IPhoneticConverter phoneticConverter,
             IStringDistanceEvaluator distanceEvaluator, IVariableProvider variableProvider,
@@ -37,23 +40,54 @@ namespace GoogleSpeechApi.TextProcessing
             _logger.Info($"Input: {input}");
             _logger.Info($"Transliterated: {transliteratedInput}");
             _logger.Info($"Phonetic: {phoneticInput}");
-            var distances = CalculateDistances(variables, phoneticInput);
-            var sortedDistances = distances
+            var distances = CalculatePhoneticDistances(variables, phoneticInput);
+            List<KeyValuePair<VariableRepresentation, int>> sortedDistances = distances
                 .ToList().OrderBy(pair => pair.Value)
                 .ToList();
-            sortedDistances.ForEach(kvp => _logger.Info($"{kvp.Key.Name}({kvp.Key.PhoneticName}) - {kvp.Value}"));
+            sortedDistances.Take(5).ForEach(kvp => _logger.Info($"{kvp.Key.Name}({kvp.Key.PhoneticName}) - {kvp.Value}"));
             _logger.Info("=========================================");
-            return sortedDistances.First().Key.Name;
+            List<KeyValuePair<VariableRepresentation, int>> phoneticDistances = sortedDistances
+                .Where(kvp => kvp.Value == sortedDistances.Min(pair => pair.Value))
+                .ToList();
+            if (phoneticDistances.Count == 1)
+            {
+                return phoneticDistances.First().Key.Name;
+            }
+
+            var textDistances = CalculateTextDistances(phoneticDistances, transliteratedInput)
+                .OrderBy(pair => pair.Value).ToList();
+            _logger.Info($"Transliterated: {transliteratedInput}");
+            textDistances.Take(5).ForEach(kvp => _logger.Info($"{kvp.Key.Name} - {kvp.Value}"));
+            if (textDistances.First().Value > MaxPlainTextAllowedDistance)
+                return phoneticDistances.First().Key.Name;
+            return textDistances.First().Key.Name;
+            //sortedDistances.First().Key.Name;
         }
 
-        private Dictionary<VariableRepresentation, int> CalculateDistances(List<VariableRepresentation> variables,
+        private Dictionary<VariableRepresentation, int> CalculatePhoneticDistances(List<VariableRepresentation> variables,
             string phoneticInput)
         {
             var res = new Dictionary<VariableRepresentation, int>(variables.Count);
             foreach (var variableRepresentation in variables)
             {
-                res[variableRepresentation] = _distanceEvaluator.GetDistance(variableRepresentation.PhoneticName, phoneticInput);
+                res[variableRepresentation] =
+                    _distanceEvaluator.GetDistance(variableRepresentation.PhoneticName, phoneticInput);
             }
+
+            return res;
+        }
+
+        private Dictionary<VariableRepresentation, int> CalculateTextDistances(
+            IEnumerable<KeyValuePair<VariableRepresentation, int>> variables, string transliteratedInput)
+        {
+            var variablesList = variables.ToList();
+            var res = new Dictionary<VariableRepresentation, int>(variablesList.Count);
+            foreach (var variableRepresentation in variablesList)
+            {
+                res[variableRepresentation.Key] =
+                    _distanceEvaluator.GetDistance(variableRepresentation.Key.Name, transliteratedInput);
+            }
+
             return res;
         }
     }
--- VoiceCodeWpf/MainWindow.xaml.cs
+++ VoiceCodeWpf/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GoogleSpeechApi.Context.Interfaces;
 using GoogleSpeechApi.DI;
 using GoogleSpeechApi.Grammars;
@@ -18,8 +20,9 @@ namespace VoiceCodeWpf
     /// </summary>
     public partial class MainWindow
     {
-        private readonly ISpeechRecongizer _recongizer = new GoogleSpechRecognizerWrapper();
+        private readonly ISpeechRecongizer _recongizer = Kernel.Instance.Get<GoogleSpechRecognizerWrapper>();
         private readonly Grammar _grammar;
+        private string lastRecognized;
 
         public MainWindow()
         {
@@ -27,19 +30,25 @@ namespace VoiceCodeWpf
             Kernel.Instance.Load(new DI.DiModule());
 
             _grammar = new Grammar(Kernel.Instance.Get<IIdeContext>());
+            //_grammar.Rules.Add(GrammarRuleBuilder.Get
+            //    .FromString("Валера ест огурцы")
+            //    .WithCommand(new PrintCommand(AppendCode))
+            //    .Build());
+            //_grammar.Rules.Add(GrammarRuleBuilder.Get
+            //    .FromString("переменная <text>")
+            //    .WithCommand(new PrintCommand(AppendCode))
+            //    .Build());
+            //
             _grammar.Rules.Add(GrammarRuleBuilder.Get
-                .FromString("Валера ест огурцы")
-                .WithCommand(new PrintCommand(AppendCode))
-                .Build());
-            _grammar.Rules.Add(GrammarRuleBuilder.Get
-                .FromString("переменная <text>")
+                .FromString("<text>")
                 .WithCommand(new PrintCommand(AppendCode))
                 .Build());
         }
 
         private void AppendCode(string text)
         {
-            AppendToEditor(CodeEditor, text);
+            lastRecognized = text;
+            AppendToEditor(CodeEditor, text + Environment.NewLine);
         }
 
         private void AppendToEditor(TextEditor editor, string text)
@@ -57,6 +66,17 @@ namespace VoiceCodeWpf
             _recongizer.OnError += (o, args) => { AppendToEditor(TextEditorHistory, args.Exception.ToString()); };
 //r.OnSpeechRecognized += (o, args) => { AppendToEditor(args.Text); };
             _recongizer.StartRecognitionAsync();
+        }
+
+        private void EvalRecognitionEventHandle(object sender, KeyEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(lastRecognized))
+            {
+                if (e.Key == Key.Right)
+                    File.AppendAllLines(@"Log/correct.txt", new[] {lastRecognized});
+                if (e.Key == Key.Left)
+                    File.AppendAllLines(@"Log/incorrect.txt", new[] {lastRecognized});
+            }
         }
     }
 }

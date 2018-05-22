996e644462230b8adb9d916f25c9c5762cdd97cf
fraz <diman896@gmail.com>
2018-05-22 21:28:47 +0300

    60 sec recognition started implementing workaround

--- GoogleSpeechApi/Recognizer/GoogleSpechRecognizerWrapper.cs
+++ GoogleSpeechApi/Recognizer/GoogleSpechRecognizerWrapper.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Google.Cloud.Speech.V1;
+using GoogleSpeechApi.Context.Provider;
 using GoogleSpeechApi.Recognizer.Interfaces;
 using NAudio.Wave;
 
@@ -10,9 +13,13 @@ namespace GoogleSpeechApi.Recognizer
 {
     public class GoogleSpechRecognizerWrapper : ISpeechRecongizer
     {
-        private SpeechClient.StreamingRecognizeStream _streamingRecognizeStream;
+        private List<SpeechClient.StreamingRecognizeStream> streams = new List<SpeechClient.StreamingRecognizeStream>();
         private bool _writeMore = true;
         private readonly object _writeLock = new object();
+        private readonly IVariableProvider _variableProvider;
+        private bool isRecording = false;
+
+        private SpeechClient.StreamingRecognizeStream GetCurrentStream => streams.Last();
 
         private readonly WaveInEvent _waveInEvent = new WaveInEvent
         {
@@ -21,6 +28,11 @@ namespace GoogleSpeechApi.Recognizer
         };
 
         private SpeechClient _speechClient;
+
+        public GoogleSpechRecognizerWrapper(IVariableProvider variableProvider)
+        {
+            _variableProvider = variableProvider;
+        }
 
         private static StreamingRecognizeRequest GetStreamingRecognizeRequest(SpeechContext speechContext)
         {
@@ -53,20 +65,22 @@ namespace GoogleSpeechApi.Recognizer
                 }
 
                 _speechClient = SpeechClient.Create();
-                _streamingRecognizeStream = _speechClient.StreamingRecognize();
+                var stream = _speechClient.StreamingRecognize();
+                streams.Add(stream);
                 var speechContext = new SpeechContext();
                 speechContext.Phrases.AddRange(new[]
-                    {"int", "for", "true", "false", "public", "private", "bool", "static", "void", "переменная"});
+                        {"int", "for", "true", "false", "public", "private", "bool", "static", "void", "переменная"}
+                    /*.Concat(_variableProvider.GetVariables().Select(v => v.Name))*/);
                 // Write the initial request with the config.
                 StreamingRecognizeRequest recognizeRequest = GetStreamingRecognizeRequest(speechContext);
-                await _streamingRecognizeStream.WriteAsync(recognizeRequest);
+                await stream.WriteAsync(recognizeRequest);
                 // Print responses as they arrive.
 
                 Task printResponses = Task.Run(async () =>
                 {
-                    while (await _streamingRecognizeStream.ResponseStream.MoveNext(default(CancellationToken)))
+                    while (await stream.ResponseStream.MoveNext(default(CancellationToken)))
                     {
-                        foreach (StreamingRecognitionResult streamingRecognitionResult in _streamingRecognizeStream
+                        foreach (StreamingRecognitionResult streamingRecognitionResult in stream
                             .ResponseStream
                             .Current.Results)
                         {
@@ -79,9 +93,7 @@ namespace GoogleSpeechApi.Recognizer
                     }
                 });
                 // Read from the microphone and stream to API.
-
-                _waveInEvent.DataAvailable += NewMethod;
-                _waveInEvent.StartRecording();
+                ActivateMicrophone();
                 Console.WriteLine("Speak now.");
                 //await Task.Delay(TimeSpan.FromSeconds(seconds));
                 // Stop recording and shut down.
@@ -90,7 +102,7 @@ namespace GoogleSpeechApi.Recognizer
                 //await printResponses;
                 return 0;
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Debug.WriteLine(e);
             }
@@ -98,14 +110,24 @@ namespace GoogleSpeechApi.Recognizer
             return -1;
         }
 
-        private void NewMethod(object sender, WaveInEventArgs args)
+        private void ActivateMicrophone()
+        {
+            if (!isRecording)
+            {
+                _waveInEvent.DataAvailable += WriteData;
+                _waveInEvent.StartRecording();
+                isRecording = true;
+            }
+        }
+
+        private void WriteData(object sender, WaveInEventArgs args)
         {
             lock (_writeLock)
             {
                 if (!_writeMore) return;
                 try
                 {
-                    _streamingRecognizeStream.WriteAsync(
+                    GetCurrentStream.WriteAsync(
                         new StreamingRecognizeRequest
                         {
                             AudioContent = Google.Protobuf.ByteString
@@ -115,8 +137,6 @@ namespace GoogleSpeechApi.Recognizer
                 catch (Exception e)
                 {
                     OnError?.Invoke(this, new SpeechRecognizerErrorEventArgs(e));
-                    StopRecognition();
-                    StartRecognitionAsync();
                 }
             }
         }
@@ -128,10 +148,7 @@ namespace GoogleSpeechApi.Recognizer
         {
             StreamingMicRecognizeAsync().ConfigureAwait(false);
             //            StreamingMicRecognizeAsync(100);
-            OnSpeechRecognized += (o, a) =>
-            {
-                Debug.WriteLine(a.Text);
-            };
+            OnSpeechRecognized += (o, a) => { Debug.WriteLine(a.Text); };
         }
 
         public void StopRecognition()
@@ -141,7 +158,7 @@ namespace GoogleSpeechApi.Recognizer
 
             try
             {
-                _streamingRecognizeStream.WriteCompleteAsync().Wait();
+                GetCurrentStream.WriteCompleteAsync().Wait();
             }
             catch
             {
@@ -152,7 +169,6 @@ namespace GoogleSpeechApi.Recognizer
         public async Task StartRecognition()
         {
             await StreamingMicRecognizeAsync();
-
         }
     }
 }
